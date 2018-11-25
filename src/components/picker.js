f288017452da1f677756d4c777e4bfb09037070e
Kayden Forthman <forthmankayden@gmail.com>
2018-11-25 09:39:24 -0700

    installed moment.js, React date Picker, and placed picker component component grid

---
+++ src/components/picker.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import DatePicker from "react-datepicker";
+import moment from 'moment';
+
+
+class Picker extends component {
+    render() {
+        constructor(props) {
+            super(props);
+            this.state = {
+                startDate: moment()
+            };
+            this.handleChange = this.handleChange.bind(this);
+        }
+
+        handleChange(date) {
+            console.log('trying to change date for', date._d)
+            this.setState({
+                startDate: date
+            });
+        }
+        return (
+
+            <div className="Picker">
+                <DatePicker
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
+            </div>
+        )
+    }
+}
+export default Picker;
